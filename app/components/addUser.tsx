e363cfdfecaf96620ce3905321111d5cec05608e
gentleatif <coderatif@gmail.com>
2024-12-23 17:52:32 +0530

    first commit

---
+++ app/components/addUser.tsx
@@ -0,0 +1,142 @@
+"use client";
+import React, { useState } from "react";
+
+import styles from "./addUser.module.css";
+
+interface AddUserFormProps {
+  onUserAdded: () => void;
+}
+
+const AddUserForm: React.FC<AddUserFormProps> = ({ onUserAdded }) => {
+  const url =
+    "http://ec2-13-233-246-112.ap-south-1.compute.amazonaws.com:3001/user";
+  const [newUser, setNewUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+  const [profileImage, setProfileImage] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setNewUser((prev) => ({ ...prev, [name]: value }));
+    setErrors((prev) => ({ ...prev, [name]: "" }));
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setProfileImage(e.target.files[0]);
+    }
+  };
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = { firstName: "", lastName: "", email: "" };
+    if (newUser.firstName.length < 3 || newUser.firstName.length > 20) {
+      newErrors.firstName = "First name must be between 3 and 20 characters.";
+      valid = false;
+    }
+    if (newUser.lastName.length < 3 || newUser.lastName.length > 20) {
+      newErrors.lastName = "Last name must be between 3 and 20 characters.";
+      valid = false;
+    }
+    if (!/\S+@\S+\.\S+/.test(newUser.email)) {
+      newErrors.email = "Invalid email address.";
+      valid = false;
+    }
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleAddUser = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser), // Sending the new user data
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to add user");
+      }
+
+      onUserAdded(); // Refresh the user list
+      setNewUser({
+        firstName: "",
+        lastName: "",
+        email: "",
+      });
+      setProfileImage(null);
+    } catch (error) {
+      console.error("Error adding user:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form className="user-form" onSubmit={handleAddUser}>
+      <h2>Add New User</h2>
+      <div className="form-group">
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={newUser.firstName}
+          onChange={handleInputChange}
+          placeholder="Enter first name"
+          className={styles.userInput}
+        />
+        {errors.firstName && <p className={styles.error}>{errors.firstName}</p>}
+      </div>
+      <div className="form-group">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={newUser.lastName}
+          onChange={handleInputChange}
+          placeholder="Enter last name"
+          className={styles.userInput}
+        />
+        {errors.lastName && <p className={styles.error}>{errors.lastName}</p>}
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={newUser.email}
+          onChange={handleInputChange}
+          placeholder="Enter email"
+          className={styles.userInput}
+        />
+        {errors.email && <p className={styles.error}>{errors.email}</p>}
+      </div>
+
+      <button type="submit" className="submit-button" disabled={isLoading}>
+        {isLoading ? "Loading..." : "Add User"}
+      </button>
+    </form>
+  );
+};
+
+export default AddUserForm;
---
+++ app/users/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import AddUserForm from "../components/addUser";
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  profileImageUrl: string;
+}
+
+const UserPage = () => {
+  const url =
+    "http://ec2-13-233-246-112.ap-south-1.compute.amazonaws.com:3001/user";
+
+  // const url = "http://localhost:3001/user";
+  const [users, setUsers] = useState<User[]>([]);
+  // const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const fetchUsers = () => {
+    // setLoading(true); // Start loading
+    fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        return response.json(); // Parse JSON
+      })
+      .then((data) => {
+        setUsers(data); // Update state with fetched users
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+        setError("Some Error Occurs"); // Store error
+      })
+      .finally(() => {
+        setLoading(false); // End loading
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []); // Empty dependency array ensures this runs only once after the component mounts
+
+  if (loading) return <p>Loading users...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  const handleDelete = (userId: string) => {
+    console.log(`Delete user with ID: ${userId}`);
+
+    fetch(`${url}/${userId}`, {
+      method: "DELETE",
+    }).then((res) => {
+      console.log(res);
+      fetchUsers();
+    });
+  };
+
+  const handleUpload = (userId: string, file: File) => {
+    console.log(`Upload file for user with ID: ${userId}`, file);
+  };
+
+  return (
+    <div className="container">
+      <AddUserForm onUserAdded={fetchUsers} /> {/* Add the form here */}
+      <h1>User List</h1>
+      <ul className="user-list">
+        {users.map((singleUser) => (
+          <li className="user-card" key={singleUser._id}>
+            {/* <img src={singleUser.profileImageUrl} alt={singleUser.firstName} /> */}
+            <div className="user-details">
+              <h3>
+                {singleUser.firstName} {singleUser.lastName}
+              </h3>
+              <p>{singleUser.email}</p>
+            </div>
+            <div className="button-group">
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(singleUser._id)}
+              >
+                Delete
+              </button>
+              {/* <label
+                htmlFor={`upload-${singleUser._id}`}
+                className="upload-label"
+              >
+                Upload Image
+              </label> */}
+              <input
+                type="file"
+                id={`upload-${singleUser._id}`}
+                className="upload-input"
+                onChange={(e) =>
+                  e.target.files &&
+                  handleUpload(singleUser._id, e.target.files[0])
+                }
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserPage;
